75072e8fc35ab8c078217ba88d592c9fae856410
YuHua, Wu <je18573@gmail.com>
2015-09-05 15:02:11 +0800

    [Gulp] 在 mytask1 的 fn 加入 "cb" 參數，並在 fn 最後將 cb(); 註解

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('default', ['mytask1', 'mytask2'], function() {
 //!!! 若傳入cb, 則一定要在function中加上cb()，否則此task不執行。
 gulp.task('mytask1', function(cb) {
 	console.log('Task1 Output');
-	cb();
+	//cb();
 });
 
 gulp.task('mytask2', function(cb) {
@@ -15,14 +15,18 @@ gulp.task('mytask2', function(cb) {
 });
 /*
 執行結果：
-[14:57:30] Using gulpfile ~\Code\f2e-workflow-labs\gulpfile.js
-[14:57:30] Starting 'mytask1'...
+[14:58:54] Using gulpfile ~\Code\f2e-workflow-labs\gulpfile.js
+[14:58:54] Starting 'mytask1'...
 Task1 Output
-[14:57:30] Finished 'mytask1' after 2.44 ms
-[14:57:30] Starting 'mytask2'...
+[14:58:54] Starting 'mytask2'...
 Task2 Output
-[14:57:30] Finished 'mytask2' after 172 μs
-[14:57:30] Starting 'default'...
-My Default Task
-[14:57:30] Finished 'default' after 277 μs
+[14:58:54] Finished 'mytask2' after 238 μs
  */
+ 
+ /*
+ 解釋：
+ 因是採非同步處理，因此mytask1, mytask2同時執行，
+ 但mytask1因有傳cb但無執行cb()，會導致一直沒有完成，
+ 再因相依關係，mytask1&mytask2要同時跑完才會執行default的log，
+ 而mytask1無完成,所以不會跑出My Default task。
+*/
