8ab55f2d0c857630eb8b9eeafc9817626ab4ecce
YuHua, Wu <je18573@gmail.com>
2015-09-05 14:56:47 +0800

    [Gulp] 在 mytask1 的 fn 加入 "cb" 參數，並在 fn 最後執行 cb();

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ gulp.task('default', ['mytask1', 'mytask2'], function() {
 	console.log('My Default Task');
 });
 
-gulp.task('mytask1', function() {
+//!!! 若傳入cb, 則一定要在function中加上cb()，否則此task不執行。
+gulp.task('mytask1', function(cb) {
 	console.log('Task1 Output');
+	cb();
 });
 
 gulp.task('mytask2', function() {
